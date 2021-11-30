133a2cdeb2bd788287c96daa272a432234d108fd
matmagamb <ceifa13@outlook.com>
2021-11-30 13:03:17 -0300

    ambientes

--- reactJs/01-github-explore/src/App.jsx
+++ reactJs/01-github-explore/src/App.jsx
@@ -1,4 +1,4 @@
 export function App(){
-    throw new Error('eita giovana o forninho caiu' );
-    return <h1>Hello world reactjs05 </h1>
+   // throw new Error('eita giovana o forninho caiu' );
+    return <h1>Hello world reactjs06https://webpack.js.org/configuration/mode/ </h1>
 }
